8398f89f63eb08595fe778f916eb92773380eccb
Eduardo Guerra <eduardo.antares@gmail.com>
2021-04-26 23:49:44 +0000

    Improve env file loading.

--- resources/bootstrap/env.php
+++ resources/bootstrap/env.php
@@ -1,16 +1,30 @@
 <?php
 
-//-- customize the env file
-$app->useEnvironmentPath($app->basePath() . DIRECTORY_SEPARATOR . 'env');
+//-- customize the env file loading
+
+$app->useEnvironmentPath(rtrim($app->basePath(), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'env');
 
 $envId = '';
+
+if ($app->runningInConsole()) {
+    $consoleArgs = new \Symfony\Component\Console\Input\ArgvInput();
+    if ($consoleArgs->hasParameterOption('--env')) {
+        $envId = $consoleArgs->getParameterOption('--env');
+    }
+}
+
 if (isset($_SERVER['HTTP_APP_ENV_ID'])) {
     $envId = $_SERVER['HTTP_APP_ENV_ID'];
 }
+
 if (empty($envId) and isset($_ENV['APP_ENV_ID'])) {
     $envId = $_ENV['APP_ENV_ID'];
 }
 
-$envId = !empty($envId) ? ".{$envId}" : '.local';
+$envFile = $app->environmentPath() . DIRECTORY_SEPARATOR . '.env.' . (!empty($envId) ? "{$envId}" : 'default');
 
-$app->loadEnvironmentFrom(".env{$envId}");
+if (!file_exists($envFile)) {
+    throw new Exception("File not found: {$envFile}");
+}
+
+$app->loadEnvironmentFrom(basename($envFile));
